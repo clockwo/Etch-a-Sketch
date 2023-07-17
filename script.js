68685d22abaaaa7c9bd01c8977705477eb1de559
clockwo <39350729+clockwo@users.noreply.github.com>
2023-07-17 12:07:05 +0300

    feat: :sparkles: Add grid for 16, 32, 64, 128

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const screenPainter = document.querySelector('.screen__painter');
 
-for (let i = 0; i < 100; i++) {
+const width = screenPainter.getBoundingClientRect().width;
+const GRID_VALUE = 128;
+
+// console.log(Math.floor(width / GRID_VALUE));
+
+for (let i = 1; i <= GRID_VALUE * GRID_VALUE; i++) {
   const newSquare = document.createElement('div');
   newSquare.classList.add('square');
+  if (i % GRID_VALUE === 0) {
+    newSquare.style.background = 'red';
+  }
+  newSquare.style.width = `${Math.round(width / GRID_VALUE)}px`;
+  // newSquare.style.width = `${width / GRID_VALUE}px`;
   screenPainter.appendChild(newSquare);
 }
